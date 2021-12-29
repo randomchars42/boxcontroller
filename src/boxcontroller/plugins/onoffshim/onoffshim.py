5e5dc3bd5269d2aecf041ef9a125a4dde8dbe114
randomchars42 <randomchars42@users.noreply.github.com>
2021-12-29 23:22:25 +0100

    try to fix detection

--- src/boxcontroller/plugins/onoffshim/onoffshim.py
+++ src/boxcontroller/plugins/onoffshim/onoffshim.py
@@ -18,18 +18,20 @@ class Onoffshim(ProcessPlugin):
         ProcessPlugin.__init__(self, *args, **kwargs)
         self.__chip = kwargs['main'].get_config().get('OnOffShim',
                 'chip', default=None, variable_type='int')
-        self.__pin_shutdown = kwargs['main'].get_config().get('OnOffShim',
-                'pin', default=4, variable_type='int')
+        self.__pin['shutdown'] = kwargs['main'].get_config().get('OnOffShim',
+                'pin_shutdown', default=4, variable_type='int')
+        self.__pin['listen'] = kwargs['main'].get_config().get('OnOffShim',
+                'pin_listen', default=17, variable_type='int')
         self.__time_pressed = 0
-        if self.__chip is None or self.__pin_shutdown is None:
+        if self.__chip is None or self.__pin_listen is None:
             logger.error('No chip or pins defined')
             return
 
     def get_chip(self):
         return self.__chip
 
-    def get_pin_shutdown(self):
-        return self.__pin_shutdown
+    def get_pin(self, type):
+        return self.__pin[type]
 
     def on_pressed(self, pin):
         logger.debug('GPIO {} pressed'.format(pin))
@@ -41,18 +43,18 @@ class Onoffshim(ProcessPlugin):
         logger.debug('GPIO {} held for {} seconds'.format(pin, diff))
         if diff > 1:
             logger.debug('triggering shutdown')
-            #logger.debug('pulling down shutdown pin')
-            ## pull down the pin so the button can trigger next boot
-            #with gpiod.Chip(self.get_chip()) as chip:
-            #    lines = chip.get_lines([self.get_pin_shutdown()])
-            #    lines.request(consumer='boxcontroller.plugins.onoffshim',
-            #            type=gpiod.LINE_REQ_DIR_OUT)
-            #    lines.set_values([0])
+            logger.debug('pulling down shutdown pin')
+            # pull down the pin so the button can trigger next boot
+            with gpiod.Chip(self.get_chip()) as chip:
+                lines = chip.get_lines([self.get_pin('shutdown')])
+                lines.request(consumer='boxcontroller.plugins.onoffshim',
+                        type=gpiod.LINE_REQ_DIR_OUT)
+                lines.set_values([0])
             self.queue_put('shutdown')
 
     def run(self):
         monitor = GPIODMonitor(self.get_chip())
-        monitor.register(self.get_pin_shutdown(),
+        monitor.register(self.get_pin('listen')(),
             on_pressed = lambda pin: self.on_pressed(pin),
             on_released = lambda pin: self.on_released(pin))
         logger.debug('listening to shutdown pin')
